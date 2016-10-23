ada2f808191f1ac9fdf98bb3665dcb3c20c78a84
rcholic <ivytony@gmail.com>
2016-10-23 19:52:15 -0400

    modified nav header

---
+++ MultiMediaNotes/NotesListViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NotesListViewController.swift
+//  MultiMediaNotes
+//
+//  Created by Guoliang Wang on 10/23/16.
+//  Copyright © 2016 iParroting.com. All rights reserved.
+//
+
+import UIKit
+
+class NotesListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+    
+    func setupUI() {
+        // set up nav bar
+        self.navigationController?.navigationBar.barTintColor = UIColor.init(red: 0, green: 0.24, blue: 0.45, alpha: 1)
+        self.navigationController?.navigationBar.tintColor = UIColor.white
+        self.title = "Lecture Notes"
+        let textShadow = NSShadow()
+        textShadow.shadowColor = UIColor.init(red: 0, green: 0, blue: 0, alpha: 0.8)
+        textShadow.shadowOffset = CGSize(width: 0, height: 1)
+        let fontAttr = UIFont(name: "HelveticaNeue-CondensedBlack", size: 25)
+        self.navigationController?.navigationBar.titleTextAttributes = NSDictionary(objects: [UIColor.white, textShadow, fontAttr!], forKeys: [NSForegroundColorAttributeName as NSCopying, NSShadowAttributeName as NSCopying, NSFontAttributeName as NSCopying]) as? [String : AnyObject]
+    }
+
+}
---
+++ Podfile
@@ -0,0 +1,22 @@
+# Uncomment this line to define a global platform for your project
+platform :ios, '8.1'
+
+use_frameworks!
+
+target 'MultiMediaNotes' do
+  # Comment this line if you're not using Swift and don't want to use dynamic frameworks
+    pod 'SnapKit'
+    pod 'SwiftDate', '~> 4.0'
+
+  # Pods for MultiMediaNotes
+
+  target 'MultiMediaNotesTests' do
+    #inherit! :search_paths
+  end
+
+  target 'MultiMediaNotesUITests' do
+    #inherit! :search_paths
+    # Pods for testing
+  end
+
+end
